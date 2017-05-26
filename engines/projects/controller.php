cc776420b131b053ce81badfdfbcf8c8fc6f7979
Donald Mkpanam <donald.mkpanam@gmail.com>
2017-05-26 07:08:36 +0100

    Fixed engines and models

--- engines/projects/controller.php
+++ engines/projects/controller.php
@@ -30,6 +30,10 @@ class CController extends ZController{
 
 	function view() {
 		$projects = new Project;
+		if(isset($_POST["projectid_blocks"])) {
+			$projects->create_new_block();
+			$this->redirect("projects", "view", $_POST["projectid_blocks"]);
+		}	
 		$id = $this->uri->id;
 		$tmp = $this->app->tmp();
 		$project = $projects->get_project($id);
@@ -38,8 +42,43 @@ class CController extends ZController{
 		$tmp["project_name"] = $project->name;
 		$tmp["description"] = $project->description;
 		$tmp["location"] = $project->site_loc;
+		$tmp["projectid"] = $project->id;
+		$tmp["projectblocks"] = $projects->get_blocks($id);
+		$tmp["projectunits"] = $projects->get_project_units($id);
 		self::render("view", $tmp);
 	}
+
+
+	function new_unit(){
+		$projects = new Project;
+		$blockid = $this->uri->id;
+		if(isset($_POST["unit_description"])) {		
+			$projects->create_new_unit();
+			$this->redirect("projects", "view", $_POST["projectid"]);
+		}
+		$projectid = $projects->get_pid_from_bid($blockid);
+		$tmp = $this->app->tmp();
+		$tmp["unittypes"] = $projects->get_unit_types();
+		$tmp["page_title"] = "New Unit";
+		$tmp["page_subtitle"] = "Enter New User";
+		$tmp["blockid"] = $blockid;
+		$tmp["projectid"] = $projectid;
+		self::display("new_user", $tmp);
+	}
+
+	function edit_block(){
+		$projects = new Project;
+		$blockid = $this->uri->id;
+		if(isset($_POST["unit_description"])) {		
+			$projects->create_new_unit();
+			$this->redirect("projects", "view", $_POST["projectid"]);
+		}
+		$tmp = $this->app->tmp();
+		$tmp["page_title"] = "Edit Block";
+		$tmp["page_subtitle"] = "Enter Existing Block";
+		self::display("edit_block", $tmp);
+	}
+
 
 	
 
--- models/projects.php
+++ models/projects.php
@@ -45,6 +45,112 @@ class Project extends Zedek{
 		return $result;
 	}
 
+	public function create_new_block() {
+		$db = self::orm()->pdo;
+		$q ="insert into project_blocks (project, name, description) values (:project, :name, :description)";
+		try {
+			$query = $db->prepare($q);
+			$query->bindParam(':name', $_POST["name"]);
+			$query->bindParam(':description', $_POST["description"]);
+			$query->bindParam(':project', $_POST["projectid_blocks"]);
+			$query->execute();
+		} catch (Exception $e) {
+			echo "Cannot create project block".$e->getMessage();
+		}
+	}
+
+	public function create_new_unit() {
+		$db = self::orm()->pdo;
+		$q ="insert into blocks_units (block, unit_type, unit_description) values (:block, :unit_type, :unit_description)";
+		try {
+			$query = $db->prepare($q);
+			$query->bindParam(':block', $_POST["blockid"]);
+			$query->bindParam(':unit_type', $_POST["unit_type"]);
+			$query->bindParam(':unit_description', $_POST["unit_description"]);
+			$query->execute();
+		} catch (Exception $e) {
+			echo "Cannot create project block".$e->getMessage();
+		}
+	}
+
+	public function get_blocks($project) {
+		$db = self::orm()->pdo;
+		$q ="select project_blocks.id as bid, name, description, (select count(id) from blocks_units where blocks_units.block = bid) as units
+		from project_blocks where project = :project";
+		try {
+			$query = $db->prepare($q);
+			$query->bindParam(':project', $project);
+			$query->execute();
+		} catch (Exception $e) {
+			echo "Cannot get Projec Blocks".$e->getMessage();
+		}
+		$result = $query->fetchAll();
+		return $result;
+
+	}
+
+	public function get_block($id) {
+		$db = self::orm()->pdo;
+		$q ="select project_blocks.id as bid, name, description, (select count(id) from blocks_units where blocks_units.block = bid) as units
+		from project_blocks where id = :id";
+		try {
+			$query = $db->prepare($q);
+			$query->bindParam(':id', $id);
+			$query->execute();
+		} catch (Exception $e) {
+			echo "Cannot get Projec Blocks".$e->getMessage();
+		}
+		$result = $query->fetchObject();
+		return $result;
+
+	}
+
+	public function get_project_units($project) {
+		$db = self::orm()->pdo;
+		$q ="select blocks_units.id as unitid, blocks_units.unit_description as unitdescription, project_blocks.name as blockname,
+		unit_types.name as unittype from blocks_units join project_blocks on blocks_units.block = project_blocks.id
+		join unit_types on blocks_units.unit_type = unit_types.id
+		where project_blocks.project = :project";
+		try {
+			$query = $db->prepare($q);
+			$query->bindParam(':project', $project);
+			$query->execute();
+		} catch (Exception $e) {
+			echo "Cannot get Project Units".$e->getMessage();
+		}
+		$result = $query->fetchAll();
+		return $result;
+
+	}
+
+	public function get_unit_types() {
+		$db = self::orm()->pdo;
+		$q ="select id, name from unit_types";
+		try {
+			$query = $db->prepare($q);
+			$query->execute();
+		} catch (Exception $e) {
+			echo "Cannot get unit types.".$e->getMessage();
+		}
+		$result = $query->fetchAll();
+		return $result;
+	}
+
+	public function get_pid_from_bid($blockid) {
+		$db = self::orm()->pdo;
+		$q ="select project from project_blocks where id = :id";
+		//echo
+		try {
+			$query = $db->prepare($q);
+			$query->bindParam(':id', $blockid);
+			$query->execute();
+		} catch (Exception $e) {
+			echo "Cannot create project block".$e->getMessage();
+		}
+		$result = $query->fetchObject();
+		return $result->project;
+	}
+
 	
 	
 
